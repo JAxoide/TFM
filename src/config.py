b47f1c3f43185e10f1941cea84498722fbc4ed01
JAxoide <hunts395@gmail.com>
2025-09-24 20:37:20 +0200

    Update config.py

--- src/config.py
+++ src/config.py
@@ -4,6 +4,6 @@ from dotenv import load_dotenv
 # Cargar variables de entorno desde un archivo .env
 load_dotenv()
 
-OPENAI_API_KEY = os.getenv("OPENAI_API_KEY", "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")
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY", "introduce clave de OPEN AI")
 MODEL_NAME = "gpt-4-turbo"
 VECTOR_DB_PATH = "data/vector_db"
